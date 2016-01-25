41f5ce85502fab5280ede14fbee595c706fa3d11
pranjan77 <pranjan77@gmail.com>
2016-01-25 10:06:26 -0500

    Update vimsetup.sh

--- vimsetup.sh
+++ vimsetup.sh
@@ -2,3 +2,14 @@
 apt-get install -y vim
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+Look at this file for working with Nerd tree
+i: split horizontally
+gi: i and cursor and nerd tree
+
+s: split vertically
+gs: cursor at nerd tree
+
+
+
+https://github.com/scrooloose/nerdtree/blob/master/doc/NERD_tree.txt
